d14956eda76c34f22c1f4b154a08ead1c9f0c3d1
Carlos Perales <carlosperales050@gmail.com>
2025-04-18 17:51:36 -0600

    generando los modelos con Entity Framework

---
+++ WebApi/WebApi/Models/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.Models;
+
+public partial class Post
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string Content { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<Comment> Comments { get; set; } = new List<Comment>();
+
+    public virtual ICollection<Reaction> Reactions { get; set; } = new List<Reaction>();
+
+    public virtual User User { get; set; } = null!;
+}
--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,9 +1,18 @@
+using System;
 using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 //add varibales .env
 Env.Load("../.env");
 
+
+// variable para cadena de conection 
+var connectionString = builder.Configuration.GetConnectionString("Connection");
+
+// registrar servicio para la conexion (** para controllers )
+builder.Services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connectionString));
 // Add services to the container.
 
 builder.Services.AddControllers();
