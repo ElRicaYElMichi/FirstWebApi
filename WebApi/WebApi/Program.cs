9010cc23dbe331598a633f17ae180cf8fbe84492
Carlos Perales <carlosperales050@gmail.com>
2025-04-30 19:17:39 -0600

    migrando de dotNetEnv a secrets

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,11 +1,7 @@
 using System;
-using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using WebApi.Models;
 var builder = WebApplication.CreateBuilder(args);
-
-//add varibales .env
-Env.Load("../.env");
 
 
 // variable para cadena de conection 
